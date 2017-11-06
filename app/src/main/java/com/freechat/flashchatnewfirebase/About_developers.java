7bdc6e5711b56cafae956eec0adf5739b1bcd4f2
amanckc <aman96.mit@gmail.com>
2017-11-06 22:37:00 +0530

    finallly all edits done

--- app/src/main/java/com/freechat/flashchatnewfirebase/About_developers.java
+++ app/src/main/java/com/freechat/flashchatnewfirebase/About_developers.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class About_developers extends AppCompatActivity {
 Button b1;
     ImageView i1,i2;
+    //TextView t1,t2;
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
@@ -17,6 +19,7 @@ Button b1;
         b1= (Button) findViewById (R.id.Back);
         i1= (ImageView) findViewById (R.id.abhi);
         i2= (ImageView) findViewById (R.id.aman);
+
         b1.setOnClickListener (new View.OnClickListener () {
             @Override
             public void onClick (View v) {
